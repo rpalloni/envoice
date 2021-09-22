a00d098ea4acaf1123a0fb904244254363fd1e94
rpalloni <rpdev20@gmail.com>
2021-09-22 18:30:59 +0200

    add tests

--- drf/core/invoice/tests.py
+++ drf/core/invoice/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from models import Invoice
+from .serializers import InvoiceSerializer
+# run test: python manage.py test
+
+
+class InvoiceViewSetTestCase(APITestCase):
+
+    def setUp(self):
+        pass
+
+    def api_authentication(self):
+        pass
+
+    def test_invoices(self):
+        response = self.client.get("/api/v1/invoices/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
